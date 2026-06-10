2b5c17e2a5e4734e386f9f2185b6b1576c8acae7
Maria Gorrasi <maria_gorrasi@hotmail.com>
2026-06-10 11:29:47 +0200

    functies toegevoegd

--- tijdelijk.py
+++ tijdelijk.py
@@ -1,12 +1,27 @@
-prijzen = {"aardbei":3, "vanille":4,"chocolade":5}
-aanbieding = prijzen ["aardbei"] * 0.8
-reclame_tekst = (f"Vandaag in de aanbieding: vanilleijs, 1 liter - slechts € {aanbieding}")
-#De index van de eerste 0 na de komma is 63, dus we knippen de tekst af bij index 63
-reclame_tekst2 = reclame_tekst[:63]
-reclame_tekst3 = reclame_tekst2.upper()
-reclame_tekst4 = reclame_tekst3.split(" ")
-for el in reclame_tekst4:
-    if len(el) >= 5:
-        print(el.upper())
-else:
-        print(el.lower())
+from helper import decoreer
+
+def print_aanbieding():
+    prijzen = {
+        "aardbei":3, 
+        "vanille":4,
+        "chocolade":5
+    }
+
+    aanbieding = prijzen ["aardbei"] * 0.8
+    
+    reclame_tekst = (f"Vandaag in de aanbieding: vanilleijs, 1 liter - slechts € {aanbieding}")
+    
+    reclame_tekst2 = reclame_tekst[:63]
+    
+    reclame_tekst3 = reclame_tekst2.upper()
+    
+    reclame_tekst4 = reclame_tekst3.split(" ")
+    
+    for el in reclame_tekst4:
+        if len(el) >= 5:
+            print(el.upper())
+        else:
+            print(el.lower())
+
+decoreer("Aanbieding")
+print_aanbieding()      
